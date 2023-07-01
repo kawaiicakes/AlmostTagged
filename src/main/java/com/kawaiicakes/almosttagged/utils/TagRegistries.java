7977b14e2cb43a4d99caf57a2dd5e2fcae40551b
kawaiicakes <vomitmopper@gmail.com>
2023-07-01 17:11:51 -0600

    TagRegistries class

---
+++ src/main/java/com/kawaiicakes/almosttagged/utils/TagRegistries.java
@@ -0,0 +1,7 @@
+package com.kawaiicakes.almosttagged.utils;
+
+public class TagRegistries {
+    /* https://docs.minecraftforge.net/en/1.19.x/resources/server/tags/
+    would have been nice to know this shit existed sooner...
+     */
+}
