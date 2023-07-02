bd60f75dac8b1e6b1519d469b1d8bb9175569300
kawaiicakes <vomitmopper@gmail.com>
2023-07-02 17:32:44 -0600

    Added TagReference class

--- src/main/java/com/kawaiicakes/almosttagged/utils/TagReference.java
+++ src/main/java/com/kawaiicakes/almosttagged/utils/TagReference.java
@@ -9,6 +9,13 @@ import net.minecraftforge.registries.tags.ITag;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+/*
+VERY crude implementation. Iterating through the entire registry of
+tags seems unideal; and possibly laggy.
+
+Additionally merging these two methods might be more elegant
+of a solution. My concern is readability.
+ */
 class TagReference {
     static Stream<TagKey<Item>> getItemTagStreamFromItem(Item item) {
         return Objects.requireNonNull(ForgeRegistries.ITEMS.tags())
