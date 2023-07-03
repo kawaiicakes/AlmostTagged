2cbd7f2ebfe23d9fd628f9de0f24fd95cf4f552b
kawaiicakes <vomitmopper@gmail.com>
2023-07-03 08:19:14 -0600

    handles item datagen

--- src/main/java/com/kawaiicakes/almosttagged/datagen/ItemTagGenerator.java
+++ src/main/java/com/kawaiicakes/almosttagged/datagen/ItemTagGenerator.java
@@ -1,19 +1,41 @@
 package com.kawaiicakes.almosttagged.datagen;
 
+import com.kawaiicakes.almosttagged.api.AlmostUnifiedLookupWrapper;
+import com.kawaiicakes.almosttagged.utils.TagReference;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.ForgeRegistryTagsProvider;
 import net.minecraftforge.registries.IForgeRegistry;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class ItemTagGenerator extends ForgeRegistryTagsProvider<Item>{
-    public ItemTagGenerator(DataGenerator generator, IForgeRegistry<Item> forgeRegistry, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+    protected ItemTagGenerator(DataGenerator generator, IForgeRegistry<Item> forgeRegistry, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(generator, forgeRegistry, modId, existingFileHelper);
     }
 
     @Override
     protected void addTags() {
-        //shit i'll finish this later lmao got stuff to do
+        final Set<TagKey<Item>> tagSet = AlmostUnifiedLookupWrapper.getConfiguredTags();
+        assert tagSet != null;
+
+        for (TagKey<Item> key : tagSet) {
+            final Item preferredItem = AlmostUnifiedLookupWrapper.getPreferredItemForTag(key);
+            if (preferredItem.toString().equals("minecraft:air")) return;
+
+            final Set<Item> potentialItems = Objects.requireNonNull(AlmostUnifiedLookupWrapper.getPotentialItems(key));
+            if (potentialItems.size() == 0) return;
+
+            final Stream<TagKey<Item>> tagStream = potentialItems
+                    .stream()
+                    .mapMulti((item, consumer) -> TagReference.getItemTagStreamFromItem(item).forEach(consumer));
+
+            tagStream.forEach(iTag -> tag(iTag).add(preferredItem));
+        }
     }
 }
