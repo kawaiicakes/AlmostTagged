833a2fbf566448888083ea5a769c33f7b81583f5
kawaiicakes <vomitmopper@gmail.com>
2023-07-05 12:08:54 -0600

    changed to only assign value to a field in TagStreamGenerators

--- src/main/java/com/kawaiicakes/almosttagged/mixins/RecipeManagerMixin.java
+++ src/main/java/com/kawaiicakes/almosttagged/mixins/RecipeManagerMixin.java
@@ -1,6 +1,7 @@
 package com.kawaiicakes.almosttagged.mixins;
 
 import com.google.gson.JsonElement;
+import com.kawaiicakes.almosttagged.api.AlmostUnifiedLookupWrapper;
 import com.kawaiicakes.almosttagged.tags.TagStreamGenerators;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -16,10 +17,9 @@ import java.util.Map;
 //priority 1500 bc i don't wanna be near kjs or au :skull:
 @Mixin(value = RecipeManager.class, priority = 1_500)
 public class RecipeManagerMixin {
-    //This simply injects calls to methods in TagStreamGenerators while ensuring AU configs have a chance to load first
+    //This mixin simply assigns a value to TagStreamGenerators.tagSet while ensuring AU configs have a chance to load first
     @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
-    private void runTransformation(Map<ResourceLocation, JsonElement> recipes, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
-        TagStreamGenerators.getAUItemTagStream();
-        TagStreamGenerators.getAUBlockTagStream();
+    private void apply(Map<ResourceLocation, JsonElement> recipes, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
+        TagStreamGenerators.tagSet(AlmostUnifiedLookupWrapper.getConfiguredTags());
     }
 }
