f9cd3d9a3e526021b58ad41e745fe9ad627059b3
kawaiicakes <vomitmopper@gmail.com>
2023-07-09 14:07:10 -0600

    added warning in json file

--- src/main/java/com/kawaiicakes/almosttagged/config/ConfigEntries.java
+++ src/main/java/com/kawaiicakes/almosttagged/config/ConfigEntries.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class ConfigEntries {
+    public String Warning;
     public Map<String, Set<String>> itemTagJsonMap = new HashMap<>();
     public Map<String, Set<String>> blockTagJsonMap = new HashMap<>();
 
