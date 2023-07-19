11db269b9373e1dad0d98d1886f5c00ef2ed9f22
kawaiicakes <vomitmopper@gmail.com>
2023-07-18 19:59:57 -0600

    TagLoaderAPI is a class designed to interact with TagLoader & instances of TagData<V> to allow for more effective and readable modification & reading of tags.

--- src/main/java/com/kawaiicakes/almosttagged/api/TagLoaderAPI.java
+++ src/main/java/com/kawaiicakes/almosttagged/api/TagLoaderAPI.java
@@ -1,9 +1,11 @@
 package com.kawaiicakes.almosttagged.api;
 
-import com.kawaiicakes.almosttagged.AlmostTagged;
+import com.almostreliable.unified.AlmostUnified;
+import com.almostreliable.unified.api.AlmostUnifiedLookup;
 import com.kawaiicakes.almosttagged.tags.TagData;
-import com.kawaiicakes.almosttagged.tags.TagUtils;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,39 +13,86 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
+/**
+ * This class is made to allow simpler interaction with the tags from <code>TagLoader</code>,
+ * whose data is instantiated in <code>TagData&lt;V&gt;</code>.
+ */
 public class TagLoaderAPI {
 
-    private static TagData<Item> itemTagData; //these work and return tags loading onto stuff including from datapack edits
+    private static TagData<Holder.Reference<Item>> itemTagData; //these work and return tags loading onto stuff including from datapack edits
     @SuppressWarnings("unchecked")
-    public static void setItemTagData(TagData<?> tagData) {itemTagData = (TagData<Item>) tagData;}
-    public static TagData<Item> getItemTagData() {return itemTagData;}
+    public static <V> void setItemTagData(TagData<V> tagData) {itemTagData = (TagData<Holder.Reference<Item>>) tagData;}
 
-    private static TagData<Block> blockTagData;
+    private static TagData<Holder.Reference<Block>> blockTagData;
     @SuppressWarnings("unchecked")
-    public static void setBlockTagData(TagData<?> tagData) {blockTagData = (TagData<Block>) tagData;}
-    public static TagData<Block> getBlockTagData() {return blockTagData;}
+    public static <V> void setBlockTagData(TagData<V> tagData) {blockTagData = (TagData<Holder.Reference<Block>>) tagData;}
 
-    @SuppressWarnings({"unchecked", "OptionalGetWithoutIsPresent"})
+    /**
+     * This is by far the longest and most complex method in the entire module. In spite of that,
+     * it's not actually *that* bad. When <code>#unifyTags</code> is called, the <code>itemTagData</code>
+     * and <code>blockTagData</code> fields are modified.
+     * <p>
+     * These modifications require <code>AlmostUnified#isRuntimeLoaded</code> to be <code>true</code> as
+     * many of the necessary calls to the Almost Unified API will not function if the AU runtime is not
+     * initialized.
+     * <p>
+     * For every tag that AU intends to unify, there are items (the 'potential items') which have the tag,
+     * and a preferred item which AU will use to unify entries. This method simply gets the potential items
+     * bound per tag to be unified, gets the tags (the 'tag pool') associated with the potential items, and
+     * then modifies the corresponding field such that the preferred item is now bound to all tags in the
+     * tag pool. It also excludes any tags or items specified in <code>TagConfig</code>.
+     */
+    public static void unifyTags() {
+        if (!AlmostUnified.isRuntimeLoaded()) throw new RuntimeException();
+        final AlmostUnifiedLookup INST = AlmostUnifiedLookup.INSTANCE;
+        final Block AIR = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("minecraft:air"));
+
+        for (TagKey<Item> tag : INST.getConfiguredTags()) {
+            Holder.Reference<Item> preferredItemHolder = Objects.requireNonNull(INST.getPreferredItemForTag(tag))
+                    .builtInRegistryHolder(); //Item#builtInRegistryHolder is deprecated but whatever lmao (Block too)
+
+            INST.getPotentialItems(tag)
+                    .stream()
+                    .<TagKey<?>>mapMulti((item, consumer) -> itemTagData.getTags(item.builtInRegistryHolder())
+                            .forEach(consumer))
+                    .map(TagKey::location)
+                    .forEach(resourceLocation -> itemTagData.add(resourceLocation, preferredItemHolder));
+
+            if (Block.byItem(INST.getPreferredItemForTag(tag)) != AIR) {
+                Holder.Reference<Block> preferredBlockHolder = Block.byItem(INST
+                                .getPreferredItemForTag(tag))
+                                .builtInRegistryHolder();
+
+                INST.getPotentialItems(tag)
+                        .stream()
+                        .<Block>mapMulti((item, consumer) -> {
+                            if (Block.byItem(item) != AIR) {
+                                consumer.accept(Block.byItem(item));
+                            }
+                        })
+                        .<TagKey<?>>mapMulti((block, consumer) -> blockTagData.getTags(block.builtInRegistryHolder())
+                                .forEach(consumer))
+                        .map(TagKey::location)
+                        .forEach(resourceLocation -> blockTagData.add(resourceLocation, preferredBlockHolder));
+            }
+        }
+
+        itemTagData.print();
+        blockTagData.print();
+    }
+
     public static <T> void modifyReturn(CallbackInfoReturnable<Map<ResourceLocation, Collection<T>>> map,
                                         @NotNull String directory) {
         switch (directory) {
             case "tags/items":
-                AlmostTagged.StorageConfig.itemTagJsonMap.forEach((k, v) -> v.forEach(stringTag ->
-                        map.getReturnValue().put(TagUtils.getTagResourceLocationFromString(stringTag),
-                                (Collection<T>) Collections.singleton(ForgeRegistries.ITEMS
-                                        .getHolder(new ResourceLocation(k)).get()))));
-                AlmostTagged.LOGGER.info("Almost Tagged has successfully inherited item tags.");
+
                 break;
 
             case "tags/blocks":
-                AlmostTagged.StorageConfig.blockTagJsonMap.forEach((k, v) -> v.forEach(stringTag ->
-                        map.getReturnValue().put(TagUtils.getTagResourceLocationFromString(stringTag),
-                                (Collection<T>) Collections.singleton(ForgeRegistries.BLOCKS
-                                        .getHolder(new ResourceLocation(k)).get()))));
-                AlmostTagged.LOGGER.info("Almost Tagged has successfully inherited block tags.");
+
                 break;
         }
     }
--- src/main/java/com/kawaiicakes/almosttagged/mixins/TagLoaderMixin.java
+++ src/main/java/com/kawaiicakes/almosttagged/mixins/TagLoaderMixin.java
@@ -1,12 +1,9 @@
 package com.kawaiicakes.almosttagged.mixins;
 
-import com.kawaiicakes.almosttagged.AlmostTagged;
 import com.kawaiicakes.almosttagged.api.TagLoaderAPI;
 import com.kawaiicakes.almosttagged.tags.TagData;
-import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagLoader;
-import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Mixin(value = TagLoader.class, priority = 25_000)
 public class TagLoaderMixin {
@@ -32,11 +28,10 @@ public class TagLoaderMixin {
     @Inject(method = "build(Ljava/util/Map;)Ljava/util/Map;", at = @At("RETURN"))
     private <T> void build(Map<ResourceLocation, List<TagLoader.EntryWithSource>> p_203899_, CallbackInfoReturnable<Map<ResourceLocation, Collection<T>>> map) {
         if (directory.equals("tags/items")) {
-            TagLoaderAPI.setItemTagData(new TagData<>());
-            TagLoaderAPI.getItemTagData().forEach((k, v) -> AlmostTagged.LOGGER.info(k.toString() + ": " + v.stream().map(t -> ((Holder.Reference<T>) t).get()).collect(Collectors.toSet())));
+            TagLoaderAPI.setItemTagData(new TagData<>(map.getReturnValue()));
             //TagLoaderAPI.modifyReturn(map, directory);
         } else if (directory.equals("tags/blocks")) {
-            TagLoaderAPI.setBlockTagData(new TagData<>());
+            TagLoaderAPI.setBlockTagData(new TagData<>(map.getReturnValue()));
             //TagLoaderAPI.modifyReturn(map, directory);
         }
     }
