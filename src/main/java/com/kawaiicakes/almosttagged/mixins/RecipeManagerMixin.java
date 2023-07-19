e089633873dd44e492749d3482c8a25f6e1b6cdc
kawaiicakes <vomitmopper@gmail.com>
2023-07-18 20:12:46 -0600

    TagLoaderAPI is a class designed to interact with TagLoader & instances of TagData<V> to allow for more effective and readable modification & reading of tags.

--- src/main/java/com/kawaiicakes/almosttagged/mixins/RecipeManagerMixin.java
+++ src/main/java/com/kawaiicakes/almosttagged/mixins/RecipeManagerMixin.java
@@ -1,8 +1,7 @@
 package com.kawaiicakes.almosttagged.mixins;
 
 import com.google.gson.JsonElement;
-import com.kawaiicakes.almosttagged.api.AlmostUnifiedLookupWrapper;
-import com.kawaiicakes.almosttagged.tags.TagCollections;
+import com.kawaiicakes.almosttagged.api.TagLoaderAPI;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
@@ -17,9 +16,8 @@ import java.util.Map;
 //priority 1500 bc i don't wanna be near kjs or au :skull:
 @Mixin(value = RecipeManager.class, priority = 1_500)
 public class RecipeManagerMixin {
-    //This mixin simply assigns a value to TagCollections.tagSet while ensuring AU configs have a chance to load first
     @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
     private void apply(Map<ResourceLocation, JsonElement> recipes, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
-        TagCollections.tagSet(AlmostUnifiedLookupWrapper.getConfiguredTags()); //generateAUTags must be called after this is fired, but also after tags load
+        TagLoaderAPI.unifyTags(); //AU's Runtime API is initialized at the spot this is injected.
     }
 }
--- src/main/java/com/kawaiicakes/almosttagged/tags/TagUtils.java
+++ src/main/java/com/kawaiicakes/almosttagged/tags/TagUtils.java
@@ -1,14 +1,11 @@
 package com.kawaiicakes.almosttagged.tags;
 
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
-import java.util.stream.Stream;
 
 /**
  * This class provides static utility methods for tags,
@@ -26,74 +23,10 @@ import java.util.stream.Stream;
  * it may already be complex. It will also simplify debugging.
  */
 public class TagUtils {
-
-    /**
-     * This is a simple <code>boolean</code> field indicating whether <code>ITagManager&lt;V&gt;</code>
-     * has loaded or not.
-     * <p>
-     * <code>false</code> by default until <code>#tagsLoaded</code> is called during the appropriate time.
-     */
-    private static boolean TAGS_LOADED = false;
-
-    /**
-     * When called, indicates <code>ITagManager&lt;V&gt;</code> has successfully loaded.
-     */
-    public static void tagsLoaded() {
-        TAGS_LOADED = true;
-    }
-
-    /**Simple method returning a <code>Stream</code> of <code>TagKey&lt;Item&gt;</code>
-     * corresponding to the <code>Item</code> of a given <code>ResourceLocation</code>.
-     * <p>
-     * A <code>ResourceLocation</code> is taken as a param as it's simply what is easiest
-     * to work with in this context. A <code>Stream</code> is also kept as the return
-     * to allow potential operations on the data prior to collecting it.
-     *
-     * @param resourceLocation  the <code>ResourceLocation</code> used to resolve tags.
-     * @return      a <code>Stream</code> containing <code>TagKey&lt;Item&gt;</code>.
-     * @throws AssertionError   thrown when this method is called before
-     *                          <code>ITagManager&lt;V&gt;</code> has loaded.
-     * @throws NullPointerException thrown when the argument points to no valid <code>Item</code> or
-     *                              if for whatever Godforsaken reason <code>IForgeRegistry&lt;Item&gt;</code>
-     *                              does not support tags/has a wrapper registry.
-     */
-    public static @NotNull Stream<TagKey<Item>> itemTagsFromHolder(ResourceLocation resourceLocation) {
-        if (!TAGS_LOADED) throw new AssertionError();
-        final Item item = Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(resourceLocation));
-
-        return Objects.requireNonNull(ForgeRegistries.ITEMS.tags()).getReverseTag(item).orElseThrow().getTagKeys();
-    }
-
-    /**
-     * Basically the same as <code>#itemTagsFromHolder</code>, but using <code>Block</code>
-     * in lieu of <code>Item</code>.
-     *
-     * @param resourceLocation  the <code>ResourceLocation</code> used to resolve tags.
-     * @return      a <code>Stream</code> containing <code>TagKey&lt;Block&gt;</code>.
-     * @throws AssertionError   thrown when this method is called before
-     *                          <code>ITagManager&lt;V&gt;</code> has loaded.
-     * @throws NullPointerException thrown when the argument points to no valid <code>Block</code> or
-     *                              if for whatever Godforsaken reason <code>IForgeRegistry&lt;Block&gt;</code>
-     *                              does not support tags/has a wrapper registry.
-     */
-    public static @NotNull Stream<TagKey<Block>> blockTagsFromHolder(ResourceLocation resourceLocation) {
-        if (!TAGS_LOADED) throw new AssertionError();
-        final Block block = Objects.requireNonNull(ForgeRegistries.BLOCKS.getValue(resourceLocation));
-
-        return Objects.requireNonNull(ForgeRegistries.BLOCKS.tags()).getReverseTag(block).orElseThrow().getTagKeys();
-    }
-
-    public static @NotNull ResourceLocation getTagResourceLocationFromString(@NotNull String string) {
-        String location = string.replaceAll(".+/", "").strip().substring(0, string.replaceAll(".+/", "").strip().indexOf("]")).strip();
-        return new ResourceLocation(location);
-    }
-
-    public static @NotNull TagKey<Item> getItemTagKeyFromResource(ResourceLocation location) {
-        return new TagKey<>(ForgeRegistries.ITEMS.getRegistryKey(), location);
-    }
-
-    public static @NotNull TagKey<Block> getBlockTagKeyFromResource(ResourceLocation location) {
-        return new TagKey<>(ForgeRegistries.BLOCKS.getRegistryKey(), location);
+    public static <V> @NotNull TagKey<?> getTagKeyFromResourceLocation(@NotNull V v, ResourceLocation location) {
+        return (((Holder.Reference<?>) v).get() instanceof Item)
+                ? new TagKey<>(ForgeRegistries.ITEMS.getRegistryKey(), location)
+                : new TagKey<>(ForgeRegistries.BLOCKS.getRegistryKey(), location);
     }
 
 }
