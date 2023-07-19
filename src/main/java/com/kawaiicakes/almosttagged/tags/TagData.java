3eb1888c2534620ea040f7e1d97001225c7405b8
kawaiicakes <vomitmopper@gmail.com>
2023-07-18 20:12:06 -0600

    Converted to Record from a Class, in addition to removing unnecessary/redundant methods. Other optimizations.

--- src/main/java/com/kawaiicakes/almosttagged/tags/TagData.java
+++ src/main/java/com/kawaiicakes/almosttagged/tags/TagData.java
@@ -1,13 +1,14 @@
 package com.kawaiicakes.almosttagged.tags;
 
+import com.kawaiicakes.almosttagged.AlmostTagged;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,40 +16,65 @@ import java.util.stream.Stream;
  * <code>TagLoader#build</code>. This map contains keys of type <code>ResourceLocation</code> and
  * values of <code>Collection</code>s of type <code>V</code>.
  * <p>
- * These keys represent a distinct tag in <code>IForgeRegistry</code> of type <code>V</code>, and
- * whose values represent the instances of <code>V</code> subtyping <code>ItemLike</code> assigned to that
- * tag.
- * @param <V>   the object type; usually intended to be either <code>Item</code>
- *              or <code>Block</code> (or more specifically the type parameter of <code>Holder.Reference&lt;V&gt;</code>).
+ * Objects of this class contain a <code>Map</code> indexing all the instances of <code>V</code> bound to
+ * a given tag; represented as a <code>ResourceLocation</code>.
+ * <p>
+ * The advantage of using generics here is apparent when considering that <code>TagKey</code>s often share
+ * locations, but not registries. Generics allow for easier referencing of a <code>TagKey</code>s in different
+ * registries but sharing a <code>ResourceLocation</code>.
+ * <p>
+ * Bear in mind that the type parameter <code>V</code> is actually given by <code>net.minecraft.tags.TagLoader</code>
+ * as <code>T</code>. This may get confusing when considering that <code>TagKey</code>s share <code>T</code>
+ * as a type parameter; <code>V extends ItemLike</code> is false whereas <code>T extends ItemLike</code> is true.
+ * Maybe this only confused me because I'm still relatively inexperienced lol.
+ * @param <V>   the type parameter; intended to be some instance of <code>Holder.Reference&lt;T&gt;</code>).
  */
-public class TagData<V> implements Map<ResourceLocation, Collection<Holder.Reference<V>>> {
+public class TagData<V> implements Map<ResourceLocation, Collection<V>>  {
 
-    private final Map<ResourceLocation, Collection<Holder.Reference<V>>> data;
+    /**
+     * This field is simply the data, specifically the map, contained in <code>this</code>>
+     * instance of the object.
+     */
+    private final Map<ResourceLocation, Collection<V>> data;
+    public TagData(Map<ResourceLocation, Collection<V>> data) {this.data = data;}
 
-    public TagData() {
-        this.data = new HashMap<>();
-    }
-    public TagData(Map<ResourceLocation, Collection<Holder.Reference<V>>> data) {
-        this.data = data;
-    }
+    public Map<ResourceLocation, Collection<V>> getData() {return this.data;}
 
-    public Map<ResourceLocation, Collection<Holder.Reference<V>>> getData() {return this.data;}
-
+    /**
+     * Method used to return information regarding what tags are to be bound to an instance of <code>V</code>. In practice this
+     * is believed to not differ from referencing tags via registries or by <code>Holder</code>.
+     * <p>
+     * The primary use of this method is to assist in returning the tags associated with all items in a tag;
+     * as in <code>TagLoaderAPI</code>.
+     *
+     * @param v an instance of type <code>V</code>; expected to be a <code>Holder.Reference</code> of
+     *          either <code>Item</code> or <code>Block</code>.
+     * @return A <code>Stream</code> of all <code>TagKey&lt;T&gt;</code>s to be bound to the instance of <code>V</code>,
+     * where <code>T</code> is the <code>#value</code> of <code>V</code>.
+     */
     public Stream<TagKey<?>> getTags(V v) {
         return this.entrySet().stream()
                 .filter(entry -> entry.getValue().contains(v))
-                .mapMulti((entry, consumer) -> {
-                    if (v.getClass() == Item.class) {
-                        consumer.accept(TagUtils.getItemTagKeyFromResource(entry.getKey()));
-                    } else {
-                        consumer.accept(TagUtils.getBlockTagKeyFromResource(entry.getKey()));
-                    }
-                });
+                .map(entry -> TagUtils.getTagKeyFromResourceLocation((v), entry.getKey()));
     }
 
-    @Nullable
-    public Stream<?> get(ResourceLocation resourceLocation) {
-        return this.data.get(resourceLocation).stream().map(e -> ((Holder.Reference<?>) e).get());
+    public void add(ResourceLocation resourceLocation, V v) {
+        Collection<V> newCol = new ArrayList<>(this.data.get(resourceLocation));
+        newCol.add(v); //this may run slow. optimize in the future
+        this.data.put(resourceLocation, newCol);
+    }
+
+    public void print() {
+        Map<String, Set<String>> printMap = new HashMap<>();
+
+        this.data.values().stream().<V>mapMulti(Iterable::forEach).collect(Collectors.toSet()).forEach(t ->
+                printMap.put(((Holder.Reference<?>) t).get().toString(), getTags(t).map(TagKey::toString)
+                        .collect(Collectors.toSet())));
+
+        printMap.forEach((k, v) -> {
+            AlmostTagged.LOGGER.info(k);
+            AlmostTagged.LOGGER.info(v.toString());
+        });
     }
 
     @Override
@@ -72,23 +98,23 @@ public class TagData<V> implements Map<ResourceLocation, Collection<Holder.Refer
     }
 
     @Override
-    public Collection<Holder.Reference<V>> get(Object key) {
+    public Collection<V> get(Object key) {
         return this.data.get(key);
     }
 
     @Nullable
     @Override
-    public Collection<Holder.Reference<V>> put(ResourceLocation key, Collection<Holder.Reference<V>> value) {
+    public Collection<V> put(ResourceLocation key, Collection<V> value) {
         return this.data.put(key, value);
     }
 
     @Override
-    public Collection<Holder.Reference<V>> remove(Object key) {
+    public Collection<V> remove(Object key) {
         return this.data.remove(key);
     }
 
     @Override
-    public void putAll(@NotNull Map<? extends ResourceLocation, ? extends Collection<Holder.Reference<V>>> m) {
+    public void putAll(@NotNull Map<? extends ResourceLocation, ? extends Collection<V>> m) {
         this.data.putAll(m);
     }
 
@@ -105,13 +131,13 @@ public class TagData<V> implements Map<ResourceLocation, Collection<Holder.Refer
 
     @NotNull
     @Override
-    public Collection<Collection<Holder.Reference<V>>> values() {
+    public Collection<Collection<V>> values() {
         return this.data.values();
     }
 
     @NotNull
     @Override
-    public Set<Entry<ResourceLocation, Collection<Holder.Reference<V>>>> entrySet() {
+    public Set<Entry<ResourceLocation, Collection<V>>> entrySet() {
         return this.data.entrySet();
     }
 }
