0785fce9e80383c14354fa71896986ecd1e09dff
kawaiicakes <vomitmopper@gmail.com>
2023-07-19 18:52:47 -0600

    Preparation of config for inheritance exclusion. Does not contain examples of/supports RegEx quite yet.

--- src/main/java/com/kawaiicakes/almosttagged/config/TagConfigEntries.java
+++ src/main/java/com/kawaiicakes/almosttagged/config/TagConfigEntries.java
@@ -1,11 +1,14 @@
 package com.kawaiicakes.almosttagged.config;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class TagConfigEntries {
-    public Map<String, Set<String>> itemTagJsonMap = new HashMap<>();
-    public Map<String, Set<String>> blockTagJsonMap = new HashMap<>();
+    public final String INFO = "The game will crash if your JSON is not formatted properly. Please make sure it is correct. Leave the value on a key blank to apply the restriction to all tags/items/blocks in that key.";
+
+    public Map<String, Set<String>> itemTagBlacklist = new HashMap<>();
+    public Map<String, Set<String>> blockTagBlacklist = new HashMap<>();
+
+    public Map<String, Set<String>> itemBlacklist = new HashMap<>();
+    public Map<String, Set<String>> blockBlacklist = new HashMap<>();
 
 }
